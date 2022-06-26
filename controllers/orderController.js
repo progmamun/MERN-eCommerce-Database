57b0ae944feae25b914663c817889737ff4339a5
progmamun <mamun333089@gmail.com>
2022-06-26 17:01:58 +0600

    show-order-route

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -66,4 +66,12 @@ const updateOrderToPaid = asyncHandler(async(req, res) => {
   }
 })
 
-export {addOrderItems, getOrderById, updateOrderToPaid}
+// @desc get logged in user orders
+// @route get /api/orders/myorders
+// @access private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({user: req.user._id})
+  res.json(orders)
+})
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders}
