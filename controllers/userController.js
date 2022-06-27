26c4839f72a57ba90eea449a4d224a5e54f6f21a
progmamun <mamun333089@gmail.com>
2022-06-27 13:13:17 +0600

    get-user-by-id-update-user-endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -124,4 +124,43 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
-export {authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser}
+// @desc get user by id
+// @route get /api/users/:id
+// @access private/admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if(user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc update user
+// @route put /api/users/:id
+// @access private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if(user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.idAdmin
+  
+  const updateUser = await user.save()
+
+  res.json({
+    _id: updateUser._id,
+    name: updateUser.name,
+    email: updateUser.email,
+    isAdmin: updateUser.isAdmin,
+  })
+} else {
+  res.status(404)
+  throw new Error('User not found')
+}
+})
+
+export {authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser}
