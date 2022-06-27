4162d081ab30318340c095b59f224e75d94411b9
progmamun <mamun333089@gmail.com>
2022-06-28 00:25:16 +0600

    create-update-product-endpoints

--- controllers/productController.js
+++ controllers/productController.js
@@ -38,4 +38,49 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct }
+// @desc create a product
+// @route post/api/products
+// @access private/admin
+const createProduct = asyncHandler(async(req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/simple.jpg',
+    brand: 'Simple brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createProduct = await product.save()
+  res.status(201).json(createProduct)
+})
+
+// @desc update a product
+// @route put /api/product/:id
+// @access private/admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {name, price, description, image, brand, category, countInStock} = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updateProduct = await product.save()
+    res.json(updateProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct }
