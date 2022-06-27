18af360a76099dda51f9e86fc33cec8719db20e9
progmamun <mamun333089@gmail.com>
2022-06-27 11:58:44 +0600

    admin-delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -109,4 +109,19 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users)
 })
 
-export {authUser, registerUser, getUserProfile, updateUserProfile, getUsers}
+// @desc delete user
+// @route delete /api/users/:id
+// @access private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if(user) {
+    await user.remove()
+    res.json({message: 'User removed'})
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export {authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser}
