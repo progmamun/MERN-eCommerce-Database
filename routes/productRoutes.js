d852a4fb8bea3382c619395b7a63d36d11387e81
progmamun <mamun333089@gmail.com>
2022-06-27 23:06:23 +0600

    admin-delete-product-api

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { getProducts, getProductById } from '../controllers/productController.js';
-const router = express.Router()
+import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js';
+import {protect, admin} from '../middleware/authMiddleware.js';
 
+const router = express.Router() // ?
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
